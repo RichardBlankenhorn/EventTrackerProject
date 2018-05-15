412670d40d0acdfd98fb2f8658a7880c54cc9f7d
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-05-14 18:11:37 -0600

    final changes prior to front end

--- CyclingRest/src/main/java/com/skilldistillery/rest/CyclingRestApplication.java
+++ CyclingRest/src/main/java/com/skilldistillery/rest/CyclingRestApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CyclingRestApplication {
+public class CyclingRestApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CyclingRestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CyclingRestApplication.class, args);
